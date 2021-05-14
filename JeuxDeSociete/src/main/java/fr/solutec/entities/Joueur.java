669a1f983511df6834c8a4ff8de392a34933e586
possart <alexis.cadoux@gmail.com>
2021-05-14 17:27:32 +0200

    Joueur Commit

--- JeuxDeSociete/src/main/java/fr/solutec/entities/Joueur.java
+++ JeuxDeSociete/src/main/java/fr/solutec/entities/Joueur.java
@@ -1,5 +1,28 @@
 package fr.solutec.entities;
 
-public class Joueur {
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+
+public class Joueur {
+	
+	@Id @GeneratedValue
+	Long id;
+	
+	@OneToOne
+	User user;
+	
 }
