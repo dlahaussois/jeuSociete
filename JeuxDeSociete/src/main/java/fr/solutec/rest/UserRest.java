11999061a92cc1109a5f456be3b037d27e371c6b
Damien <damien.lahaussois@gmail.com>
2021-05-14 16:38:50 +0200

    login and password

--- JeuxDeSociete/src/main/java/fr/solutec/rest/UserRest.java
+++ JeuxDeSociete/src/main/java/fr/solutec/rest/UserRest.java
@@ -1,5 +1,38 @@
 package fr.solutec.rest;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.solutec.entities.User;
+import fr.solutec.repository.UserRepository;
+
+@RestController @CrossOrigin
 public class UserRest {
 
+	@Autowired
+	UserRepository userRepo;
+	
+	@GetMapping("user")
+	public Iterable<User> getAllUser(){
+		return userRepo.findAll();
+	}
+	
+	@GetMapping("connect")
+	public Optional<User> connexion(@RequestBody User u){
+		return userRepo.connect(u.getLogin(), u.getPassword());
+	}
+	
+	@GetMapping("user/{id}")
+	public Optional<User> getUserById(@PathVariable Long id){
+		return userRepo.findById(id);
+	}
+	
 }
