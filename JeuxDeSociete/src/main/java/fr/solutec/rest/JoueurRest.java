09ebece1d64a12c04f4f8577da6c3896604c4b8f
possart <alexis.cadoux@gmail.com>
2021-05-15 14:35:06 +0200

    Premier commit journée

--- JeuxDeSociete/src/main/java/fr/solutec/rest/JoueurRest.java
+++ JeuxDeSociete/src/main/java/fr/solutec/rest/JoueurRest.java
@@ -1,16 +1,20 @@
 package fr.solutec.rest;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
 import fr.solutec.entities.Joueur;
-
+import fr.solutec.entities.User;
 import fr.solutec.repository.JoueurRepository;
+import fr.solutec.repository.UserRepository;
 
 @RestController @CrossOrigin("*")
 public class JoueurRest {
@@ -18,9 +22,15 @@ public class JoueurRest {
 	@Autowired
 	JoueurRepository joueurRepo;
 	
-
+	@Autowired
+	UserRepository userRepo;
+	
+	
+//Creation joueur depuis infos user
 	@PostMapping("joueur/save")
-	public Joueur creerJoueur(@RequestBody Joueur j) {
+	public Joueur addJoueur(@RequestBody User u) {
+		User u1 = userRepo.save(u);
+		Joueur j = new Joueur(null, u1);
 		return joueurRepo.save(j);
 		
 	}
@@ -29,6 +39,17 @@ public class JoueurRest {
 	public Iterable<Joueur> getAllJoueur(){
 		return joueurRepo.findAll();
 	}
+	
+
+	
+
+	
+	@GetMapping("joueur/{id}")
+	public Optional<User> getJoueurById(@PathVariable Long id){
+		return userRepo.findById(id);
+	}
+	
+
 	
 	
 }
