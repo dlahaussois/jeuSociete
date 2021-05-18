eaf31e82671848bc31f829f118d05eeb62f7f3bf
corentin <corentin.brochon@consultants-solutec.fr>
2021-05-18 23:31:33 +0200

    création d'entité, repo et rest pour le Niveau de difficulté car question : il serait plus simple de créer des tables à part pour le niveau, les ages, le nombre de joueurs etc...tout comme les catégories et les marques afin de mieux gérer les trie du catalogue par catégorie, par age, par nombre de joueurs etc...et les requetes via la barre de recherche

---
+++ JeuxDeSociete/src/main/java/fr/solutec/repository/NiveauRepository.java
@@ -0,0 +1,10 @@
+package fr.solutec.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+import fr.solutec.entities.Niveau;
+
+public interface NiveauRepository extends CrudRepository<Niveau, Long> {
+
+}
