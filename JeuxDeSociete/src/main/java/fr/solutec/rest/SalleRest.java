0eb87c3f2119d3788d9f4e59722864dde7677a74
Damien <damien.lahaussois@gmail.com>
2021-05-18 09:25:22 +0200

    Gestion des salles : afficher toutes les salles, nombre de places décrémenté

--- JeuxDeSociete/src/main/java/fr/solutec/rest/SalleRest.java
+++ JeuxDeSociete/src/main/java/fr/solutec/rest/SalleRest.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,13 +18,10 @@ import fr.solutec.repository.SalleRepository;
 
 @RestController @CrossOrigin("*") 
 public class SalleRest {
-
-
 	
 	@Autowired
 	SalleRepository salleRepo;
-	
-	
+
 	
 	@PostMapping("salle/save")
 	public Salle addSalle(@RequestBody Salle s) {
@@ -36,9 +35,6 @@ public class SalleRest {
 		return salleRepo.findAll();
 	}
 	
-
-	
-
 	
 	@GetMapping("salle/id/{id}")
 	public Optional<Salle> getSalleById(@PathVariable Long id){
@@ -51,8 +47,38 @@ public class SalleRest {
 	}
 	
 	
-
-
+	
+	@GetMapping("salle/place")
+	public int getNombreDePlacesById(@RequestBody Long id) {
+		Optional<Salle> s = getSalleById(id);
+		Salle salle = s.get();
+		if(salle!=null) {
+			return salle.getNombreDePlaces();
+		}
+		else {
+			return 0;
+		}		
+	}
+	
+	@PutMapping("salle")
+	public Salle setNombreDePlaces(@RequestBody Salle s) {
+		int nb = s.getNombreDePlaces()-1;
+		s.setNombreDePlaces(nb);
+		return salleRepo.save(s);
+	}
+	
+	@DeleteMapping("salle")
+	public boolean deleteSalle(@RequestBody Salle s) {
+		Long id = s.getId();
+		Optional<Salle> salle = salleRepo.findById(id);
+		if(salle.isPresent()) {
+			salleRepo.deleteById(id);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 	
 	
 }
