47d5ae9e99dfe77d894d594cdd7e585634a816cc
corentin <corentin.brochon@consultants-solutec.fr>
2021-05-24 17:02:09 +0200

    methode rest message

--- JeuxDeSociete/src/main/java/fr/solutec/repository/MessageRepository.java
+++ JeuxDeSociete/src/main/java/fr/solutec/repository/MessageRepository.java
@@ -20,6 +20,7 @@ public interface MessageRepository extends CrudRepository<Message, Long>{
 	public List<Message> findByForumSujet(String sujet);
 	public List<Message> findByPriveeIsFalse();
 	public List<Message> findByPriveeIsTrue();
+	public List<Message> findByMessageId(Long id);
 
 	@Query(value = "SELECT m FROM Message m inner join User u ON m.expediteur.id=u.id WHERE m.privee=false AND u.id=?1 ")
 	public Optional<Iterable<Message>> trouverPublicByExpediteurId(Long id);
